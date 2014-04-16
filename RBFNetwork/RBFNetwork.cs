6fdb763f7095c206e798a85ea130c3712b51c4b0
Denis Novogordski <novogrodski1992@gmail.com>
2014-04-16 10:27:01 +0300

    change rbf network class

--- RBFNetwork/RBFNetwork.cs
+++ RBFNetwork/RBFNetwork.cs
@@ -6,150 +6,34 @@ using System.Threading.Tasks;
 
 namespace MultilayerPerceptron
 {
-    public class RBFNetwork
-    {
+	public class RBFNetwork
+	{
+		private double[,] w;
+		private double[] centroids;
+		private double[] delta;
+		private double[] gValues;
 
-        //the next IL - input layer, HL - hidden layer, OL - outputlayer
-		private double[,] wHL;
-		private double[,] wOL;
-		private double[] valuesHL;
-		private double[] valuesOL;
-		private double[] deltaOL;
-		private readonly int _n;
-		private readonly int _p;
-		private readonly int _h;
-		private const double a = 0.8;
-		private const double Speed = 5;
-
-		public RBFNetwork(int n, int p, int h)
+		public RBFNetwork(int k)
 		{
-			_n = n;
-			_p = p;
-			_h = h;
-			PerceptronInitialization();
-            var obj = new Tuple<string, int>("asdf", 1);
-            obj.
-		}
+			centroids = new double[k];
+			delta = new double[k];
+			gValues = new double[k];
+			w = new double[k, k];
 
-		private void PerceptronInitialization()
-		{
-			wHL = new double[_n, _h];
-			wOL = new double[_h, _p];
-			valuesHL = new double[_h];
-			valuesOL = new double[_p];
-			deltaOL = new double[_p];
-
-            var r = new Random();
-
-			//wHL initialization
-			for (int i = 0; i < _n; i++)
-			{
-				for (int j = 0; j < _h; j++)
-				{
-                    wHL[i, j] = r.Next(-100, 100) / 100.0; 
-				}
-			}
-
-			//wOL initialization
-			for (int i = 0; i < _h; i++)
-			{
-				for (int j = 0; j < _p; j++)
-				{
-                    wOL[i, j] = r.Next(-100, 100) / 100.0;
-				}
-			}
-		}
-
-		private double ActivateFunction(double argument)
-		{
-			return 1 / (1 + Math.Exp(-a * argument));
-		}
-
-		private double GenerateInitialValue()
-		{
 			var r = new Random();
-			return r.Next(-100, 100) / 100.0;
-		}
 
-		public double[] GetNeuronResult(double[] input)
-		{
-			if (input.Length != _n)
+			for (var i = 0; i < k; i++)
 			{
-				throw new Exception("Incorrect input massive size!");
-			}
-
-			for (int j = 0; j < _h; j++)
-			{
-				for (int i = 0; i < _n; i++)
+				for (var j = 0; j < k; j++)
 				{
-					valuesHL[j] = ActivateFunction(GetSum(input, wHL, j));
-				}
-			}
-
-			for (int j = 0; j < _p; j++)
-			{
-				for (int i = 0; i < _h; i++)
-				{
-					valuesOL[j] = ActivateFunction(GetSum(valuesHL, wOL, j));
-				}
-			}
-
-			return valuesOL;
-		}
-
-		public void TrainedNeural(double[] input, int i)
-		{
-			double[] y;
-			var expectedResult = new double[_p];
-			expectedResult[i] = 1.0;
-
-			while (true)
-			{
-				y = GetNeuronResult(input);
-
-				if (y[i] > 0.95)
-				{
-					break;
-				}
-
-				FeedbackErrorCorrection(y, expectedResult, input);
-			}
-		}
-
-		private void FeedbackErrorCorrection(double[] y, double[] expectedResult, double[] input)
-		{
-			CorrectionOutputLayer(y, expectedResult);
-		}
-
-		private void CorrectionOutputLayer(double[] y, double[] expectedResult)
-		{
-		    for (int j = 0; j < _p; j++)
-		    {
-                deltaOL[j] = valuesOL[j] * (1 - valuesOL[j]) * (expectedResult[j] - valuesOL[j]);    
-		    }
-
-			for (int i = 0; i < _h; i++)
-			{
-				for (int j = 0; j < _p; j++)
-				{		
-					wOL[i, j] = wOL[i, j] + Speed * valuesHL[i] * deltaOL[j];
+					w[i,j] = r.Next(-100, 100) / 100.0;
 				}
 			}
 		}
 
-
-
-		private double GetSum(double[] x, double[,] matrix, int j)
+		public void SetRBFLayer(List<double[]> images)
 		{
-			double sum = 0;
-
-			for (int i = 0; i < x.Length; i++)
-			{
-				sum += matrix[i, j] * x[i];
-			}
-
-			return sum;
+			
 		}
-
-    }
+	}
 }
