2a2c225af209f6033450c7b5366a6e3910e217e1
Denis Novogordski <novogrodski1992@gmail.com>
2014-05-14 09:59:54 +0300

    add competing network class

---
+++ RBFNetwork/CompetingNetwork.cs
@@ -0,0 +1,16 @@
+﻿namespace MultilayerPerceptron
+{
+	public class CompetingNetwork
+	{
+		private double[,] w;
+		private double[2][] y;
+
+
+		public CompetingNetwork(int numberOfClasses, int size)
+		{
+			w = new double[size, numberOfClasses];
+			y = new double[];
+		}
+
+	}
+}
